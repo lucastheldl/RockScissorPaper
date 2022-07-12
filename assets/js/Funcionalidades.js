1a39f7c6f3b9a1a37ee1ac78efeed960a60ebe18
lucastheldl <lucastheldl@gmail.com>
2022-07-12 16:20:57 -0300

    first

---
+++ assets/js/Funcionalidades.js
@@ -0,0 +1,43 @@
+function ChooseOpt(num) {
+  const opt = Math.floor(Math.random() * 3) + 1;
+  let myopt = window.document.getElementById("myOpt");
+  let enemyopt = window.document.querySelector("#enemyOpt");
+  let result = window.document.querySelector("#result");
+
+  console.log(num, opt);
+
+  switch (num) {
+    case 1:
+      myopt.src = "./assets/img/Rock.png";
+      break;
+    case 2:
+      myopt.src = "./assets/img/Scissor.png";
+      break;
+    case 3:
+      myopt.src = "./assets/img/Paper.png";
+      break;
+  }
+
+  switch (opt) {
+    case 1:
+      enemyopt.src = "./assets/img/Rock.png";
+      break;
+    case 2:
+      enemyopt.src = "./assets/img/Scissor.png";
+      break;
+    case 3:
+      enemyopt.src = "./assets/img/Paper.png";
+      break;
+  }
+
+  if (num + 1 == opt) {
+    result.innerHTML = "Vitoria!";
+    result.style = "color: yellow;";
+  } else if (num == opt) {
+    result.innerHTML = "Empate!";
+    result.style = "color: white;";
+  } else {
+    result.innerHTML = "Derrota!";
+    result.style = "color: red;";
+  }
+}
